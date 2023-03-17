5bd0a2b3dc4d9981cacadbbf6784fbce7e1d2a05
davi-moura <davimoura55@gmail.com>
2023-03-16 22:06:40 -0300

    CRUD dos artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,59 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
-
-router.get("/articles", (req, res) =>{
-    res.send("rota de artigos")
+router.get("/admin/articles", (req, res) =>{
+    Article.findAll({
+        include: [{model: Category}]//join das tabelas
+    }).then(articles =>{
+        res.render("admin/articles/index",{articles: articles})
+    })
 })
 
-router.get("admin/articles/new", (req, res) =>{
-    res.send("rota para criar nova articles")
+router.get("/admin/articles/new", (req, res) =>{
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new",{categories: categories})
+    })
+})
+
+router.post("/articles/save", (req, res) =>{
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+
+})
+
+router.post("/articles/delete",(req, res)=>{
+    var id = req.body.id
+
+    if(id != undefined){
+        if(!isNaN(id)){//se for numero
+
+            Article.destroy({
+                where:{//destruir se tiver o id igual 
+                    id: id
+                }
+            }).then(() =>{//se deletar ele faz o redirect pra listagem de categorias
+                res.redirect("/admin/articles")
+            })
+
+        }else{
+            res.redirect("/admin/articles")
+        }
+    }else{//se for nulo
+        res.redirect("/admin/articles")
+    }
 })
 
 
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize")
 
 const connection = new Sequelize('bd2','root','123456',{
     host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'//dependendo do pais do servidor aonde o app vai ficar
 })
 
 
